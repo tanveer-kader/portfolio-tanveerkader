4e4b0fd834a8d7b4d48107df5bc628a4a827a72b
tanveer-kader <tanveerkaderedu@gmail.com>
2023-09-30 16:18:52 +0600

    navbar and stiky navbar fix

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ yearEl.innerHTML = `&copy; ${currentYear}`;
 // make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const navbarEl = document.querySelector(".header");
+const navlinksEL = document.querySelector(".nav-links");
 
 btnNavEl.addEventListener("click", function () {
   navbarEl.classList.toggle("nav-open");
 });
+
+// navlinksEL.addEventListener("click", function () {
+//   navbarEl.classList.remove("nav-open");
+// });
 
 ///////////////////////////////////////////////////////////
 // sticky navigation
